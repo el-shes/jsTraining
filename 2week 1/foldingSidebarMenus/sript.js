c3bc16fca1584061f6dc617d3e24e82b2bbb547d
rrfpCSIR <shes980@gmail.com>
2022-10-01 22:22:31 +0300

    folding JS sidebar menu

---
+++ 2week 1/foldingSidebarMenus/sript.js
@@ -0,0 +1,26 @@
+const submenus = document.querySelectorAll('ul li ul');
+
+function hideSubmenus() {
+  for (let i = 0; i < submenus.length; i++) {
+    submenus[i].className = 'hide-menu';
+  }
+}
+
+hideSubmenus();
+
+const menulinks = document.querySelectorAll('.menulink');
+for (let i = 0; i < menulinks.length; i++) {
+  menulinks[i].addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const thisMenu = this.parentNode.querySelector('ul');
+    // thisMenu.className = 'show-menu';
+    if (thisMenu.classList.contains('hide-menu')) {
+      hideSubmenus();
+
+      thisMenu.className = 'show-menu';
+    } else {
+      thisMenu.className = 'hide-menu';
+    }
+  });
+}
